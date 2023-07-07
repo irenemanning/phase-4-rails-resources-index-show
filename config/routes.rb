44d91d2290b346665df8e606d986000157d9dce3
Irene <irenemanningm@gmail.com>
2023-07-07 18:31:08 -0500

    Complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #More typing way to create routes
+  # get '/birds', to: 'birds#index'
+  # get '/birds/:id', to: 'birds#show'
+
+  #create ALL the restful routes
+  # resources :birds
+
+  #Create only the the routes we need
+  resources :birds, only: [:index, :show]
+
 end
